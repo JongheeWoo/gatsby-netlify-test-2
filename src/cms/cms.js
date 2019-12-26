435116298b2afffee6888b4fa3799989d2616d93
Jonghee Woo <jongheepaper@gmail.com>
2019-12-26 16:57:57 +0900

    CMS test

---
+++ src/cms/cms.js
@@ -0,0 +1,8 @@
+import CMS from "netlify-cms-app"
+import React from "react"
+
+// import PostPreview from './previews/PostPreview';
+
+// CMS.registerPreviewStyle('/styles.css')
+// CMS.registerPreviewTemplate('Post', PostPreview);
+// CMS.registerPreviewTemplate('blog', BlogPostPreview)
